6ada44c6b7fb94c45b198bffb927d6474a5e66a5
Saur0w <sthapliyal085@gmail.com>
2026-04-22 17:13:14 +0530

    "Canvas"

---
+++ src/components/Landing/canvas.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { Preload } from "@react-three/drei";
+import Obj from "./mesh";
+
+export default function Scene() {
+    return (
+        <Canvas camera={{ position: [0, 0, 5], fov: 45, near: 0.1, far: 100 }}
+                dpr={[1, 2]}
+                gl={{
+                    antialias: true,
+                    alpha: true,
+                    powerPreference: "high-performance",
+                }}
+                style={{
+                    position: "absolute",
+                    inset: 0,
+                    pointerEvents: "none",
+                }}
+                >
+            <Obj />
+            <Preload all />
+        </Canvas>
+    )
+}
