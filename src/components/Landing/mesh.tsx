1285f482b316c235f3a5c0bfcb8fb95795a58f8b
Saur0w <sthapliyal085@gmail.com>
2026-09-15 22:56:53 +0530

    feat: add landing page mesh component with custom WebGPU shaders and animations

--- src/components/Landing/mesh.tsx
+++ src/components/Landing/mesh.tsx
@@ -104,7 +104,7 @@ export default function MeshComponent() {
                 {
                     value: Math.PI,
                     duration: 3.0,
-                    ease: 'power3.inOut',
+                    ease: 'power4.inOut',
                 },
                 'flip'
             )
@@ -113,7 +113,7 @@ export default function MeshComponent() {
                 {
                     x: 2.5,
                     y: 2.5,
-                    duration: 3.0,
+                    duration: 3.4,
                     ease: 'power2.inOut',
                 },
                 'flip'
